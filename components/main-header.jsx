0e5f2641c72e67ee93f96b5e43cbb1dd39a3de4a
wuzoo <wndyd9911@gmail.com>
2024-08-15 12:56:55 +0900

    feat: next Image component for optimization

--- components/main-header.jsx
+++ components/main-header.jsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 
 import logo from "@/assets/logo.png";
+import Image from "next/image";
 import classes from "./main-header.module.css";
 
 export const MainHeader = () => {
   return (
     <header className={classes.header}>
       <Link href="/" className={classes.logo}>
-        <img src={logo.src} alt="메인 로고" />
+        <Image src={logo} alt="메인 로고" priority />
         NextLevel Food
       </Link>
 
