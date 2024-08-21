d217741a09fc44ab966b75f4a4bc7509b8f4c14b
wuzoo <wndyd9911@gmail.com>
2024-08-21 15:08:44 +0900

    refactor: loading -> loading-out.js and add Suspense streaming

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,11 +2,16 @@ import Link from "next/link";
 
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 import classes from "./page.module.css";
 
-const HomePage = async () => {
+const Meals = async () => {
   const meals = await getMeals();
 
+  return <MealsGrid meals={meals} />;
+};
+
+const HomePage = () => {
   return (
     <>
       <header className={classes.header}>
@@ -22,7 +27,11 @@ const HomePage = async () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching Meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
