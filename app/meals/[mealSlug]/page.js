3f87b27ed3c8e682b78b2d0ef19505acf9cafe7e
wuzoo <wndyd9911@gmail.com>
2024-08-21 16:38:17 +0900

    feat: call notFound when dynamic path do not exist

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,41 @@
+import Image from "next/image";
+
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
+import classes from "./page.module.css";
+
 const MealDetail = ({ params }) => {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
-    <main>
-      <h1>{params.mealSlug}</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill src={meal.image} alt={meal.title} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 };
 
---
+++ app/meals/not-found.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <main className="not-found">
+      <h1>Meal not Found</h1>
+      <p>Unfortunately, we could not found the requested page or meal data.</p>
+    </main>
+  );
+};
+
+export default NotFound;
