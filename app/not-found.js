46fc2eff2be88a6d2a33ff3102b2e7216faf3505
wuzoo <wndyd9911@gmail.com>
2024-08-21 15:32:40 +0900

    feat: create NotFound page by not-found.js

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>Unfortunately, we could not found the requested page or resource.</p>
+    </main>
+  );
+};
+
+export default NotFound;
